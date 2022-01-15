1b0eca52588bdc9d56d7764cdc944fc6144cdc33
Cole Leisen <coleleisen@gmail.com>
2022-01-15 14:31:28 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react'
+import axios from 'axios';
+import cheerio from "cheerio";
 
 function App() {
+
+  const [names, setNames] = React.useState([]);
+
+  useEffect( () => {
+    axios.get(`https://pokedex.org/`)
+    .then(function (response) {
+      console.log(response.data)
+      let namers = [];
+      let $ = cheerio.load(response.data)
+      $("li span").each(function(i,element){
+        let name = $(element)
+        .prepend()
+        .text();
+        namers.push(name);
+      })
+      console.log(namers)
+      setNames(namers);
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+},[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>hello</div>
+        <div>
+          <ul>
+          {names.length ? names.map((name, i) => {
+            return <li key={i}>{name}</li>;
+          }) : null}
+          </ul>
+        </div>
       </header>
     </div>
   );
